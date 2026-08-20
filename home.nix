bf5308a7ce3af2f0accae8b5ffd71125d9a7aa49
bentivegnaguilherme <bentivegnastudy@gmail.com>
2026-08-20 16:41:21 -0300

    feat audio: persist a2dp-only wireplumber rule + pactl

--- home.nix
+++ home.nix
@@ -7,17 +7,35 @@
        username = "gui";
        homeDirectory = "/home/gui";
        stateVersion = "26.05";
-       packages = with pkgs; [
-         kitty
-         fish
-         firefox
-         tree
-         neovim
-         git
-         opencode
-       ];
-       file.".config/niri/config.kdl".source = ./niri-config.kdl;
-     };
+packages = with pkgs; [
+          kitty
+          fish
+          firefox
+          tree
+          neovim
+          git
+          opencode
+          pulseaudio
+        ];
+        file.".config/niri/config.kdl".source = ./niri-config.kdl;
+      };
+
+    xdg.configFile."wireplumber/wireplumber.conf.d/50-bluez-a2dp.conf".text = ''
+      monitor.bluez.rules = [
+        {
+          matches = [
+            {
+              device.name = "~bluez_card.F4_4E_FC_EB_62_87$"
+            }
+          ]
+          actions = {
+            update-props = {
+              bluez5.profiles = [ a2dp_sink ]
+            }
+          }
+        }
+      ]
+    '';
 
     programs = {
       bash = {
