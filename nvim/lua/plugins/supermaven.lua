91a64468b44a37904d9170b30c26eece03d88715
bentivegnaguilherme <bentivegnastudy@gmail.com>
2026-08-20 20:49:55 -0300

    nvim: add supermaven AI autocomplete

---
+++ nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,11 @@
+return {
+  "supermaven-inc/supermaven-nvim",
+  event = "VeryLazy",
+  opts = {
+    keymaps = {
+      accept_suggestion = "<Tab>",
+      accept_word = "<C-j>",
+      clear_suggestion = "<C-]>",
+    },
+  },
+}
