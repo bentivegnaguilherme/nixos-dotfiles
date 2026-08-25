36f6a3186c49b4d4f80d7d100ae8ee432c22aee0
bentivegnaguilherme <bentivegnastudy@gmail.com>
2026-08-24 21:03:42 -0300

    feat: leetcode.nvim (kawre), snacks-picker, python3

---
+++ nvim/lua/plugins/leetcode.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "kawre/leetcode.nvim",
+    cmd = "Leet",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+    },
+    opts = {
+      lang = "python3",
+      picker = { provider = "snacks-picker" },
+      description = {
+        position = "left",
+        width = "40%",
+        show_stats = true,
+      },
+      console = {
+        open_on_runcode = true,
+        dir = "row",
+        size = { width = "90%", height = "75%" },
+        result = { size = "60%" },
+        testcase = { virt_text = true, size = "40%" },
+      },
+      keys = {
+        toggle = { "q" },
+        confirm = { "<CR>" },
+        reset_testcases = "r",
+        use_testcase = "U",
+        focus_testcases = "H",
+        focus_result = "L",
+      },
+    },
+  },
+}
