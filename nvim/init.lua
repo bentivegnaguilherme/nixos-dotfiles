80164812a09b7b9ea97e7034294c9dba29f9a466
bentivegnaguilherme <bentivegnastudy@gmail.com>
2026-08-31 22:21:27 -0300

    revert: remove F5 keymap, user just wants instructions

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- F5: run the current file in a terminal split.
-vim.keymap.set("n", "<F5>", function()
-  local file = vim.fn.expand("%:p")
-  if file == "" then
-    vim.notify("No file to run", vim.log.levels.WARN)
-    return
-  end
-  vim.cmd("split")
-  local buf = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_win_set_buf(0, buf)
-  vim.fn.termopen({ "python", file })
-  vim.cmd("startinsert")
-end, { desc = "Run current file" })
